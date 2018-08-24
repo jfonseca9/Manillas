0e244ea1dd167575f7fb0610d1f654c5b47439be
Jaime F <j.efonsecaguerrero@gmail.com>
2018-08-23 20:39:10 -0500

    parte interfaz jf

---
+++ app/src/main/java/operaciones_movil/com/tallermanillas/Manillas.java
@@ -0,0 +1,13 @@
+package operaciones_movil.com.tallermanillas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Manillas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manillas);
+    }
+}
